96ef87be08750b4402c67b2075480848a96acba6
cribbles <yaphetf@gmail.com>
2016-01-14 11:14:54 -0500

    Add eslint to repo and travis `script`

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+
+  root: true,
+
+  extends: "eslint:recommended",
+
+  parser: "babel-eslint",
+
+  env: {
+    es6: true,
+    node: true
+  },
+
+  rules: {
+    "no-bitwise": 2,
+    "curly": 2,
+    "eqeqeq": 1,
+    "guard-for-in": 2,
+    "wrap-iife": [2, "inside"],
+    "indent": [2, 2, { SwitchCase: 1 }],
+    "no-use-before-define": [2, "nofunc"],
+    "new-cap": 2,
+    "no-caller": 2,
+    "no-empty": 2,
+    "no-undefined": 2,
+    "no-unused-vars": 2,
+    "space-before-function-paren": [2, "never"],
+    "no-console": 0
+  }
+};
